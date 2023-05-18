e6cefe0ba1fd46f8d1ae9352720870e0849d249b
Kimberly Shi <kimberly.lshi@gmail.com>
2023-05-18 16:58:55 -0700

    more UI

--- cloud_site_folder/cloud_app/urls.py
+++ cloud_site_folder/cloud_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home_view, name="home_view"),
+    path('ttn/', views.ttn_view, name="ttn_view"),
     # path('', views.index, name='index'),
 ]
--- cloud_site_folder/cloud_app/views.py
+++ cloud_site_folder/cloud_app/views.py
@@ -8,5 +8,8 @@ def home_view(request):
     # https://www.thethingsindustries.com/docs/the-things-stack/interact/api/#multi-step-actions
     return render(request, "home.html")
 
+def ttn_view(request):
+    return render(request, "ttn.html")
+
 def index(request):
     return HttpResponse("Hello World")
